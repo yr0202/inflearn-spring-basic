8b21b22cd557c2e2900a8d4ad30ece9c8847f541
yr0202 <yooram0202@naver.com>
2024-08-27 02:14:51 +0900

    section7

--- core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discounPercent = 10; // 10% 할인(정률)
--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,7 +6,10 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -19,6 +22,7 @@ public class OrderServiceImpl implements OrderService {
 
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
