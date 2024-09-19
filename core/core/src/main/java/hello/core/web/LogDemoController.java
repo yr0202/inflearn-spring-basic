758549e4f5416d54fb3b9360ed326cbe66f9130d
yr0202 <yooram0202@naver.com>
2024-09-19 18:50:04 +0900

    sec10-7

--- core/core/src/main/java/hello/core/web/LogDemoController.java
+++ core/core/src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor // final이 붙은 변수로 생성자 생성/주입까지 해주는 @
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody // view가 없어서 결과를 그대로 반환하고 싶을 때 사용
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log(requestURL);
 
         myLogger.log("controller test");
