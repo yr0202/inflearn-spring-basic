59759413c504d95802449f66cb9633e0798c178e
yr0202 <yooram0202@naver.com>
2024-09-19 16:16:03 +0900

    sec9-4

--- core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -28,14 +30,14 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
-
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close()  {
         System.out.println("NetworkClient.close");
         disConnect();
